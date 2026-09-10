2c2c9051f520af969f13991a61ab7757224e87a7
sauza2016 <armar12377@gmail.com>
2026-09-10 14:05:58 -0600

    test: use test profile for application context

--- src/test/java/com/invex/employee/EmployeeServiceApplicationTests.java
+++ src/test/java/com/invex/employee/EmployeeServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.invex.employee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmployeeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
